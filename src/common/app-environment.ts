0c01133e314c8b02112f649205e34bb39529d867
rdelvallej32 <rdelvallej@gmail.com>
2017-08-25 21:34:14 -0400

    Create AppConfig service
    - App config service leverages nconf npm package to read app config
    from the base package.json to app*.json in a config folder

---
+++ src/common/app-environment.ts
@@ -0,0 +1,9 @@
+export type AppNodeEnv = 'development' | 'test' | 'prod';
+
+export const APP_NODE_ENV_DEFAULT: AppNodeEnv = 'development';
+export const APP_NODE_PORT_DEFAULT = 3000;
+
+export type AppRuntimeEnv = {
+  NODE_ENV?: AppNodeEnv;
+  PORT?: number;
+};
---
+++ src/common/services/app-config.ts
@@ -0,0 +1,51 @@
+import * as path from 'path';
+import { nconf } from '../../lib/nconf';
+
+import { AppRuntimeEnv, APP_NODE_ENV_DEFAULT, APP_NODE_PORT_DEFAULT, AppNodeEnv } from '../app-environment';
+
+type ConfigOption = {
+  type: string;
+  file: string;
+};
+
+export class NodeAppConfig {
+  constructor(public env?: AppRuntimeEnv) {
+    const pkg: Object = require(path.join(process.cwd(), 'package.json'));
+    const appEnv = {
+      NODE_ENV:  APP_NODE_ENV_DEFAULT,
+      PORT: APP_NODE_PORT_DEFAULT
+    };
+
+    nconf.argv()
+      .add('base', {type: 'literal', store: {
+        env: appEnv,
+        package: pkg
+      }})
+      .add('appenv', this.getConfigOption('app', env.NODE_ENV))
+      .add('app', this.getConfigOption('app'));
+  }
+
+  private getConfigOption(type: string, env?: AppNodeEnv): ConfigOption {
+    return {
+      type: 'file',
+      file: this.getConfigPath(type, env)
+    };
+  }
+
+  private getConfigPath(type: string, env?: AppNodeEnv): string {
+    let suffix = env ? `-${env}` : '';
+    let filename = `${type}${suffix}.json`;
+    return path.resolve(path.join(process.cwd(), 'config', filename));
+  }
+
+  public get(key?: string): any {
+    return nconf.get(key);
+  }
+
+  // Only if there are key values to be added outside of package.json
+  public set(key: string, value: string): void {
+    nconf.set(key, value);
+  }
+}
+
+export const AppConfig = new NodeAppConfig(process.env);
